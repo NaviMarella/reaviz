f4e5eb943876924b455447e8feed670a98cf1b65
Austin <amcdaniel2@gmail.com>
2020-04-01 12:00:45 -0400

    migrate map to fc, allow fill for country #11

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,35 +1,42 @@
-import React, { Component, Fragment, ReactElement } from 'react';
+import React, { Fragment, ReactElement, FC, useCallback } from 'react';
 import { geoMercator, geoPath, GeoProjection, GeoPath } from 'd3-geo';
 import {
   ChartProps,
   ChartContainer,
   ChartContainerChildProps
 } from '../common/containers/ChartContainer';
-import classNames from 'classnames';
 import { CloneElement } from '../common/utils/children';
 import { MapMarkerProps, MapMarker } from './MapMarker';
 import { motion } from 'framer-motion';
-import css from './Map.module.scss';
 
 type MarkerElement = ReactElement<MapMarkerProps, typeof MapMarker>;
 
 interface MapProps extends ChartProps {
   markers?: MarkerElement[];
   data: any;
+  fill: string;
 }
 
-export class Map extends Component<MapProps> {
-  getProjection({ chartWidth, chartHeight }: ChartContainerChildProps) {
-    return geoMercator()
-      .fitSize([chartWidth, chartHeight], this.props.data)
-      .center([0, 35]);
-  }
+export const Map: FC<MapProps> = ({
+  id,
+  width,
+  height,
+  margins,
+  className,
+  markers,
+  data,
+  fill = 'rgba(255, 255, 255, 0.3)'
+}) => {
+  const getProjection = useCallback(({ chartWidth, chartHeight }: ChartContainerChildProps) =>
+    geoMercator()
+      .fitSize([chartWidth, chartHeight], data)
+      .center([0, 35]), [data]);
 
-  renderMarker(
+  const renderMarker = useCallback((
     marker: MarkerElement,
     index: number,
     projection: GeoProjection
-  ) {
+  ) => {
     const position = projection(marker.props.coordinates);
 
     if (!position) {
@@ -47,27 +54,25 @@ export class Map extends Component<MapProps> {
         index={index}
       />
     );
-  }
+  }, []);
 
-  renderCountry(point, index: number, path: GeoPath) {
+  const renderCountry = useCallback((point, index: number, path: GeoPath) => {
     // Exclude ATA
     if (point.id === '010') {
       return null;
     }
 
     return (
-      <path key={`path-${index}`} d={path(point)!} className={css.country} />
+      <path key={`path-${index}`} d={path(point)!} fill={fill} />
     );
-  }
+  }, [fill]);
 
-  renderChart(containerProps: ChartContainerChildProps) {
-    const { markers, data } = this.props;
-
+  const renderChart = useCallback((containerProps: ChartContainerChildProps) => {
     if (!data) {
       return null;
     }
 
-    const projection = this.getProjection(containerProps);
+    const projection = getProjection(containerProps);
     const path = geoPath().projection(projection);
 
     return (
@@ -80,33 +85,29 @@ export class Map extends Component<MapProps> {
         }}
       >
         {data.features.map((point, index) =>
-          this.renderCountry(point, index, path)
+          renderCountry(point, index, path)
         )}
         {markers &&
           markers.map((marker, index) => (
             <Fragment key={`marker-${index}`}>
-              {this.renderMarker(marker, index, projection)}
+              {renderMarker(marker, index, projection)}
             </Fragment>
           ))}
       </motion.g>
     );
-  }
+  }, [data, markers]);
 
-  render() {
-    const { id, width, height, margins, className } = this.props;
-
-    return (
-      <ChartContainer
-        id={id}
-        width={width}
-        height={height}
-        margins={margins}
-        xAxisVisible={false}
-        yAxisVisible={false}
-        className={classNames(className)}
-      >
-        {props => this.renderChart(props)}
-      </ChartContainer>
-    );
-  }
+  return (
+    <ChartContainer
+      id={id}
+      width={width}
+      height={height}
+      margins={margins}
+      xAxisVisible={false}
+      yAxisVisible={false}
+      className={className}
+    >
+      {props => renderChart(props)}
+    </ChartContainer>
+  );
 }
